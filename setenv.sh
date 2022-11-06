2d38660c7fe27da8f487f2d5e58a9a136e127bf4
Yongkang He <he@yongkang.cloud>
2022-11-06 20:23:17 +0800

    Create setenv.sh

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+#-------Set the environment variables"
+export MY_CLUSTER=gke4yong1 #Customize your favorite cluster name
+export MY_MACHINE_TYPE=e2-standard-2 #Customize your favorite machine type
+export MY_REGION=us-central1 #Customize your favorite region
+export MY_ZONE=us-central1-f #Customize your favorite zone
+
