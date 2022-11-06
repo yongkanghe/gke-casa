514d9af984a115e62e544feaa57b346fb5dac0cf
Yongkang He <he@yongkang.cloud>
2022-11-06 20:23:05 +0800

    Create gke-deploy.sh

---
+++ gke-deploy.sh
@@ -0,0 +1,29 @@
+echo '-------Creating a GKE Cluster only (typically in 5 mins)'
+starttime=$(date +%s)
+. ./setenv.sh
+TEMP_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+FIRST2=$(echo -n $TEMP_PREFIX | head -c2)
+LAST2=$(echo -n $TEMP_PREFIX | tail -c2)
+MY_PREFIX=$(echo $FIRST2$LAST2)
+GKE_VERSION=$(gcloud container get-server-config --region us-central1 --flatten="channels" --filter="channels.channel=RAPID" | grep 1.25 | head -1 | awk '{print $2}')
+
+gcloud container clusters create $MY_PREFIX-$MY_CLUSTER-$(date +%s) \
+  --zone $MY_ZONE \
+  --num-nodes 2 \
+  --machine-type $MY_MACHINE_TYPE \
+  --release-channel=rapid \
+  --cluster-version $GKE_VERSION \
+  --no-enable-basic-auth \
+  --addons=GcePersistentDiskCsiDriver \
+  --enable-autoscaling --min-nodes 1 --max-nodes 3
+
+./pg-deploy.sh
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "" | awk '{print $1}'
+echo "-------Total time to build a GKE cluster is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
