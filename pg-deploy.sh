2de1e2bdf408dce6455acb5ac2dd7fc6b6b780c0
Yongkang He <he@yongkang.cloud>
2023-01-23 13:32:10 +0800

    Delete pg-deploy.sh

--- pg-deploy.sh
+++
@@ -1,5 +0,0 @@
-echo '-------Deploy a PostgreSQL sample database'
-
-kubectl create namespace yong-postgresql
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm install --namespace yong-postgresql postgres bitnami/postgresql --set primary.persistence.size=1Gi
