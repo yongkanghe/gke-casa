3be4a82f1bef8f71c61a630daa4278efe8d3980c
Yongkang He <he@yongkang.cloud>
2023-01-23 15:17:56 +0800

    Update createsa.sh

--- createsa.sh
+++ createsa.sh
@@ -1,16 +1,16 @@
-echo "-------Creating a Google Cloud Service Account"
-myproject=$(gcloud config get-value core/project)
-gcloud iam service-accounts create casa-yong-sa --display-name "casa Service Account"
-casasaemail=$(gcloud iam service-accounts list --filter "casa-yong-sa" --format="value(email)")
-gcloud iam service-accounts keys create --iam-account=${casasaemail} casa-sa-key.json
-gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${casasaemail} --role "roles/compute.storageAdmin"
-gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${casasaemail} --role "roles/storage.admin"
+# echo "-------Creating a Google Cloud Service Account"
+# myproject=$(gcloud config get-value core/project)
+# gcloud iam service-accounts create casa-yong-sa --display-name "casa Service Account"
+# casasaemail=$(gcloud iam service-accounts list --filter "casa-yong-sa" --format="value(email)")
+# gcloud iam service-accounts keys create --iam-account=${casasaemail} casa-sa-key.json
+# gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${casasaemail} --role "roles/compute.storageAdmin"
+# gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${casasaemail} --role "roles/storage.admin"
 echo "-------Enabling Google Kubernetes Engine API"
 gcloud services enable container.googleapis.com
 
 clear
 
-echo "-------Google Cloud service account is ready and GKE API enabled"
+echo "-------Google Cloud GKE API enabled"
 echo "" | awk '{print $1}'
 echo "You are ready to deploy now!"
 echo "" | awk '{print $1}'
