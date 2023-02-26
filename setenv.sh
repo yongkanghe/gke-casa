aa257f2a5c605aa7a943ffd844c3953e5f791095
Yongkang He <he@yongkang.cloud>
2023-02-26 15:55:45 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -3,5 +3,5 @@ export MY_CLUSTER=gke4yong1 #Customize your favorite cluster name
 export MY_MACHINE_TYPE=e2-standard-2 #Customize your favorite machine type
 export MY_REGION=us-central1 #Customize your favorite region
 export MY_ZONE=us-central1-f #Customize your favorite zone
-export K8S_VERSION=1.25  #Customize your GKE Version
+export K8S_VERSION=1.26  #Customize your GKE Version
 
