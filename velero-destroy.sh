5284e62a861d0fbc33458f89b778b71f7b5d1680
Yongkang He <he@yongkang.cloud>
2023-03-31 20:13:31 +1100

    Update velero-destroy.sh

--- velero-destroy.sh
+++ velero-destroy.sh
@@ -6,12 +6,18 @@ velero uninstall --force
 sudo rm /usr/local/bin/velero
 gsutil rm -r gs://$(cat bucket4velero1) -f
 rm bucket4velero1
-gcloud iam service-accounts delete $(gcloud iam service-accounts list | grep vsa4yong1 | awk '{print $2}') -q
+rm yongsa4velero1
+# export MY_PROJECT_ID=$(gcloud config get-value project)
+# MY_SERVICE_ACCOUNT_EMAIL=$(gcloud iam service-accounts list \
+#   --filter="displayName:Velero service account" \
+#   --format 'value(email)')
+# gcloud projects remove-iam-policy-binding $MY_PROJECT_ID --member=serviceAccount:$MY_SERVICE_ACCOUNT_EMAIL --role='roles/velero.server'
+# gcloud iam service-accounts delete $(gcloud iam service-accounts list | grep vsa4yong1 | awk '{print $2}') -q
 
 endtime=$(date +%s)
 duration=$(( $endtime - $starttime ))
 echo "" | awk '{print $1}'
-echo "-------Total time to build a GKE cluster with PostgreSQL is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "-------Total time to clean up the env is $(($duration / 60)) minutes $(($duration % 60)) seconds."
 echo "" | awk '{print $1}'
 echo "-------Created by Yongkang"
 echo "-------Email me if any suggestions or issues he@yongkang.cloud"
