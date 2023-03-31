055615e151e36ac9d45bf88813096c8d028e492f
Yongkang He <he@yongkang.cloud>
2023-03-31 16:09:36 +1100

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -4,4 +4,5 @@ export MY_MACHINE_TYPE=e2-standard-2 #Customize your favorite machine type
 export MY_REGION=us-central1 #Customize your favorite region
 export MY_ZONE=us-central1-f #Customize your favorite zone
 export K8S_VERSION=1.26  #Customize your GKE Version
+export MY_BUCKET=bucket4velero1	#Customize your bucket name
 
