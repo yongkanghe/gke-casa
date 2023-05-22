fa12590a170ed4dcaaa884271cd8aa9d1dbe9685
Yongkang He <he@yongkang.cloud>
2023-05-22 15:53:05 +1000

    Update postgresql-deploy.sh

--- postgresql-deploy.sh
+++ postgresql-deploy.sh
@@ -3,3 +3,4 @@ echo '-------Deploy a PostgreSQL sample database'
 kubectl create namespace yong-postgresql
 helm repo add bitnami https://charts.bitnami.com/bitnami
 helm install --namespace yong-postgresql postgres bitnami/postgresql --set primary.persistence.size=1Gi
+kubectl -n yong-postgresql annotate pod/postgres-postgresql-0 backup.velero.io/backup-volumes=data
