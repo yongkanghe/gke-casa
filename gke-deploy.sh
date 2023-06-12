6f20e96801064bbfb67364983f6285850893c427
Yongkang He <he@yongkang.cloud>
2023-06-12 20:19:56 +1000

    Update gke-deploy.sh

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -6,7 +6,7 @@ FIRST3=$(echo -n $TEMP_PREFIX | head -c3)
 LAST3=$(echo -n $TEMP_PREFIX | tail -c3)
 MY_PREFIX=$(echo $FIRST3$LAST3)
 # GKE_K8S_VERSION=$(gcloud container get-server-config --region us-central1 --flatten="channels" --filter="channels.channel=RAPID" | grep $K8S_VERSION | awk '{print $2}' | sort -r | uniq | head -1)
-GKE_K8S_VERSION=1.27.1-gke.400
+GKE_K8S_VERSION=1.27.2-gke.1200
 #GKE_K8S_VERSION=1.26.1-gke.1500
 
 gcloud container clusters create $MY_PREFIX-$MY_CLUSTER-$(date +%s) \
