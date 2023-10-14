0a32f96285b54b31d3da810f897f9f1c69e44a4b
Yongkang He <he@yongkang.cloud>
2023-10-14 10:00:06 +0700

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -3,6 +3,6 @@ export MY_CLUSTER=gke4yong1 #Customize your favorite cluster name
 export MY_MACHINE_TYPE=e2-standard-2 #Customize your favorite machine type
 export MY_REGION=us-central1 #Customize your favorite region
 export MY_ZONE=us-central1-f #Customize your favorite zone
-export K8S_VERSION=1.26  #Customize your GKE Version
+export K8S_VERSION=1.28  #Customize your GKE Version
 export MY_BUCKET=bucket4velero1	#Customize your bucket name
 export PATH=$PATH:~/gke-casa
