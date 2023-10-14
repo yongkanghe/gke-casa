4a14dfce86997125ae79ded43b853298a4e95d7c
Yongkang He <he@yongkang.cloud>
2023-10-14 10:00:01 +0700

    Update falco-deploy.sh

--- falco-deploy.sh
+++ falco-deploy.sh
@@ -2,7 +2,7 @@ helm repo add falcosecurity https://falcosecurity.github.io/charts
 helm repo update
 
 helm install falco \
-  -f https://raw.githubusercontent.com/yongkanghe/eks-casa/main/yong-k8saudit.yaml \
+  -f https://raw.githubusercontent.com/yongkanghe/gke-casa/main/yong-k8saudit.yaml \
   --set falcosidekick.enabled=true \
   --set falcosidekick.webui.enabled=true falcosecurity/falco \
   -n yong-falco \
