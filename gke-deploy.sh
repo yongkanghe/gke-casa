2da8ece56ffc34b124f4913a3921de384467645e
Yongkang He <he@yongkang.cloud>
2024-01-21 10:12:46 +1100

    Update gke-deploy.sh

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -1,4 +1,4 @@
-echo '-------Creating a GKE Cluster only (typically in ~6 mins)'
+echo '-------Creating a GKE Cluster only (typically in ~7 mins)'
 starttime=$(date +%s)
 . ./setenv.sh
 TEMP_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
