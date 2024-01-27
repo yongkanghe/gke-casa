ff8e30b3cec9b1e988b17397fe69f295b36b932a
Yongkang He <he@yongkang.cloud>
2024-01-28 09:18:07 +1100

    Update kubearmor-deploy

--- karmor-deploy.sh
+++ karmor-deploy.sh
@@ -12,4 +12,4 @@ curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin
 # Deploy a test nginx app
 kubectl create namespace yong-nginx
 kubectl create deployment nginx4yong1 --image=nginx -n yong-nginx
-POD=$(kubectl get pod -l app=nginx4yong1 -o name -n yong-nginx)
+export POD=$(kubectl get pod -l app=nginx4yong1 -o name -n yong-nginx)
