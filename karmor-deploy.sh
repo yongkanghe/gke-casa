99958f323c3f2599d5d5fa21c01590a46a4e13ed
Yongkang He <he@yongkang.cloud>
2024-01-29 18:13:21 +1100

    Update karmar cli location

--- karmor-deploy.sh
+++ karmor-deploy.sh
@@ -7,7 +7,7 @@ helm upgrade --install kubearmor-operator kubearmor/kubearmor-operator -n yong-k
 kubectl apply -f ./kubearmor-sample-config.yaml
 
 # Install KubeArmor CLI
-curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin
+curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b ~/gke-casa
 
 # Deploy a test nginx app
 kubectl create namespace yong-nginx
