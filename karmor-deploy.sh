cee66507034c789dfb224cdf83265cb48b7ecbef
Yongkang He <he@yongkang.cloud>
2024-02-02 15:55:44 +1100

    Update KubeArmor

--- karmor-deploy.sh
+++ karmor-deploy.sh
@@ -1,3 +1,6 @@
+echo '-------Installing KubeArmor on GKE Cluster (typically in ~2 mins)'
+starttime=$(date +%s)
+
 # Add helm chart repo
 helm repo add kubearmor https://kubearmor.github.io/charts 
 helm repo update kubearmor
@@ -12,3 +15,12 @@ curl -sfL http://get.kubearmor.io/ | sudo sh -s -- -b /usr/local/bin
 # Deploy a test nginx app
 kubectl create namespace yong-nginx
 kubectl create deployment nginx4yong1 --image=nginx -n yong-nginx
+
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "" | awk '{print $1}'
+echo "-------Total time to install KubeArmor with karmor CLI is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
+echo "" | awk '{print $1}'
--- karmor-destroy.sh
+++ karmor-destroy.sh
@@ -1,3 +1,14 @@
+starttime=$(date +%s)
+echo '-------Deleting the GKE Cluster (typically in few mins)'
+
 # Uninstall KubeArmor
 helm uninstall kubearmor-operator -n yong-kubearmor
 kubectl delete ns yong-nginx
+
+echo "" | awk '{print $1}'
+endtime=$(date +%s)
+duration=$(( $endtime - $starttime ))
+echo "-------Total time is $(($duration / 60)) minutes $(($duration % 60)) seconds."
+echo "" | awk '{print $1}'
+echo "-------Created by Yongkang"
+echo "-------Email me if any suggestions or issues he@yongkang.cloud"
